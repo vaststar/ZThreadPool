0fe2b1b122f32aa659002279aa5b7177ea18f6c5
thzhu <thzhu@cisco.com>
2023-03-08 16:18:00 +0800

    usage of thread pool and masterLog

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "ThreadPoolUtil.h"
+#include "LogExport.h"
+
+#include <vector>
+#include <string>
+#include <functional>
+
+#define TEST_LOG_DEBUG(message) LOG_DEBUG("TEST_LOG",message)
+int main(int argc, char* argv[])
+{
+	initConsoleLog();
+    ThreadPoolUtil::initThreadPool(5);
+    for(int i=0;i<500;++i)
+    {
+        ThreadPoolUtil::createThreadTask([i](){
+            TEST_LOG_DEBUG( "excute res:" << i*i);
+        });
+    }
+	return 0;
+}
